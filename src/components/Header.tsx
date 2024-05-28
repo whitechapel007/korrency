1c837959ea91babb9bb09c44328aad64bb393672
whitechapel007 <ebukareed@gmail.com>
2024-05-28 09:09:18 +0100

    fixed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,21 @@
-import logo from "../assets/Logo_Email_Signature.svg";
-import { CiMenuFries } from "react-icons/ci";
-
+// import logo from "../assets/Logo_Email_Signature.svg";
+import menu from "../assets/menu-04.svg";
+import secondLogo from "../assets/Korrency-white-version_Test_Landing 1.svg";
 function Header() {
   return (
-    <div className="flex items-center justify-between p-6">
-      <img src={logo} alt="" />
+    <div className="shadow-md top-0 left-0 relative bg-primary h-screen">
+      <div className="flex items-center justify-between p-6 ">
+        <img src={secondLogo} alt="" />
 
-      <CiMenuFries size={30} />
+        <div className="block md:hidden">
+          <img src={menu} alt="" />
+        </div>
+      </div>
+      <div className="bg-white p-3 absolute w-[90%] left-[35px] rounded-tl-[20px] rounded-tr-none  rounded-b-[20px] flex flex-col gap-11 px-6 py-12">
+        <ul>Company</ul>
+        <ul>Resources</ul>
+        <ul>Support</ul>
+      </div>
     </div>
   );
 }
